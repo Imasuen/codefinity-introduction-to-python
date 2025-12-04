8c3aef87dd30c0572a17a4343752a70f2fd365ab
Imasuen <>
2025-12-04 01:19:01 +0000

    String Manipulation

--- variables_and_types/string_manipulation/main.py
+++ variables_and_types/string_manipulation/main.py
@@ -1,6 +1,15 @@
 grocery_items = "milk cheese bread apples oranges chicken"
-dairy1 = print(grocery_items[0:4])
-dairy2 = print(grocery_items[5:11])
-bakery1 = print(grocery_items[12:17])
 
-message =  ("we have dairy and bakery items :"  dairy1 + "," + dairy2 + ", and" + bakery1 + "in aisle 5")
+# 1) Assign slices directly, don’t wrap in print()
+dairy1  = grocery_items[0:4]   # "milk"
+dairy2  = grocery_items[5:11]  # "cheese"
+bakery1 = grocery_items[12:17] # "bread"
+
+# 2) Concatenate into one sentence
+message = (
+    "We have dairy : "
+    + dairy1 + ", "
+    + dairy2 + ", and "
+    + bakery1
+)
+print(message)
